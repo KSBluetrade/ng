7636ac51ff20a9474f34fa2be3b444a448dc3e54
KSC <ksc@bluetrade.de>
2016-12-15 11:44:06 +0100

    Data Binding https://www.barbarianmeetscoding.com/blog/2016/03/27/getting-started-with-angular-2-step-by-step-3-your-second-component-and-angular-2-data-binding/

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -4,7 +4,6 @@ import { AppComponent } from './app.component';
 import { TestBed } from '@angular/core/testing';
 
 import { By }             from '@angular/platform-browser';
-
 ////////  SPECS  /////////////
 
 /// Delete this
@@ -30,7 +29,7 @@ describe('AppComponent with TCB', function () {
 
     let h1 = fixture.debugElement.query(el => el.name === 'h1').nativeElement;  // it works
 
-        h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
+    h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
 
     expect(h1.innerText).toMatch(/angular 2 app/i, '<h1> should say something about "Angular 2 App"');
   });

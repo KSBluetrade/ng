4cd19e115000e20bbed1f8e67b9954b8a6be0676
KSC <ksc@bluetrade.de>
2016-12-15 10:14:32 +0100

    Liste mit Personen anlegen

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Person } from './person';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>{{title}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+    title: string = "Star Wars PPlz!!!";
+}
---
+++ app/people-list.component.ts
@@ -0,0 +1,10 @@
+import { Person } from './person';
+
+export class PeopleListComponent{
+    people: Person[] = [
+        {name: 'Luke Skywalker', height: 177, weight: 70},
+        {name: 'Darth Vader', height: 200, weight: 100},
+        {name: 'Han Solo', height: 185, weight: 85},
+    ];
+
+}
